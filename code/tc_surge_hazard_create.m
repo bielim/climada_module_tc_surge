c6502f3cf65d56fc8765f8ee5bf4f0dfebea3b20
davidnbresch <david.bresch@gmail.com>
2015-01-06 19:56:10 +0100

    allows for elevation from other source
    not only ETOPO supported

--- code/tc_surge_hazard_create.m
+++ code/tc_surge_hazard_create.m
@@ -1,4 +1,4 @@
-function hazard=tc_surge_hazard_create(hazard,hazard_set_file,save_bathymetry_flag,check_plots)
+function hazard=tc_surge_hazard_create(hazard,hazard_set_file,elevation_data,check_plots)
 % climada storm surge TS hazard event set
 % NAME:
 %   tc_surge_hazard_create
@@ -7,7 +7,8 @@ function hazard=tc_surge_hazard_create(hazard,hazard_set_file,save_bathymetry_fl
 %   tropical cyclone (TC) hazard event set
 %
 %   two steps:
-%   1) obtain (high res) bathymetry, calls etopo_get
+%   1) check wether we need to obtain bathymetry (high res, calls etopo_get
+%   from module etopo) or elevation is provided (see elevation_data)
 %   2) convert all TC footprints into TS footprints
 %   see CORE_CONVERSION in code below for the conversion formula
 %
@@ -19,20 +20,29 @@ function hazard=tc_surge_hazard_create(hazard,hazard_set_file,save_bathymetry_fl
 %   see tc_surge_TEST for a testbed for this code
 %
 % CALLING SEQUENCE:
-%   hazard=tc_surge_hazard_create(hazard,hazard_set_file,save_bathymetry_flag,check_plot)
+%   hazard=tc_surge_hazard_create(hazard,hazard_set_file,elevation_data,check_plot)
 % EXAMPLE:
 %   hazard=tc_surge_hazard_create
 % INPUTS:
 %   hazard: an already existing tropical cyclone (TC) hazard event set (a
 %       TC hazard structure)
 %       > prompted for if not given (for .mat file containing a TC hazard event set)
+%       Note: if hazard.elevation_m exists on input, this elevation
+%       information is used, hence elevation_data is ignored
 %   hazard_set_file: the name of the newly created storm surge (TS) hazard
 %       event set
 %       > promted for if not given
 % OPTIONAL INPUT PARAMETERS:
-%   save_bathymetry_flag: if =1, save bathymetry in a .mat file (speeds up
-%       subsequent calls), =0 do not save bathymetry (default)
-%   check_plots: =1, do show check plots, =0: no plots (default)
+%   elevation_data: if a scalar, take elevation (or bathymetry) from etopo
+%       (needs module etopo). If =1, save bathymetry in a .mat file (speeds up
+%       subsequent calls), if =0 do not save bathymetry (default)
+%       If elevation_data is a structure with the fields lon(i), lat(i),
+%       elevation_m(i) and centroid_ID(i), check for centroid IDs being the
+%       same as in hazard.centroid_ID and then just 'attach' the elevation
+%       to hazard (i.e. hazard.elevation_m=elevation_data.elevation_m) 
+%       Note: if hazard.elevation_m exists on input, this elevation
+%       information is used.
+%   check_plots: =1, do show check plots (only if BATI used), =0: no plots (default)
 % OUTPUTS:
 %   hazard: a hazard event set, see core climada doc
 %       also written to a .mat file (see hazard_set_file)
@@ -42,6 +52,7 @@ function hazard=tc_surge_hazard_create(hazard,hazard_set_file,save_bathymetry_fl
 % David N. Bresch, david.bresch@gmail.com, 20140421
 % David N. Bresch, david.bresch@gmail.com, 20141017, module path relative
 % David N. Bresch, david.bresch@gmail.com, 20141026, save_bathymetry_flag
+% David N. Bresch, david.bresch@gmail.com, 20150106, elevation_data
 %-
 
 global climada_global
@@ -49,14 +60,17 @@ if ~climada_init_vars,return;end % init/import global variables
 
 % poor man's version to check arguments
 if ~exist('hazard','var'),hazard=[];end
-if ~exist('hazard_set_file','var'),hazard_set_file=[];end
-if ~exist('save_bathymetry_flag','var'),save_bathymetry_flag=0;end
+if ~exist('hazard_set_file','var'),hazard_set_file='';end
+if ~exist('elevation_data','var'),elevation_data=[];end
 if ~exist('check_plots','var'),check_plots=0;end
 
 % PARAMETERS
 %
 module_data_dir=[fileparts(fileparts(mfilename('fullpath'))) filesep 'data'];
 if ~isdir(module_data_dir),mkdir(fileparts(module_data_dir),'data');end % create the data dir, should it not exist (no further checking)
+%
+% whether we save the bathymetry tile for subsequent use
+save_bathymetry_flag=0; % default=0, see elevation_data
 
 if isempty(which('etopo_get'))
     fprintf('WARNING: install climada module etopo first, see https://github.com/davidnbresch/climada_module_etopo\n');
@@ -86,54 +100,86 @@ if isempty(hazard_set_file) % local GUI
     end
 end
 
-% prep the region we need (rectangular region encompassing the hazard centroids)
-centroids_rect=[min(hazard.lon) max(hazard.lon) min(hazard.lat) max(hazard.lat)];
+if ~isstruct(elevation_data)
+    save_bathymetry_flag=elevation_data;
+    elevation_data=[]; % make empty
+end
 
-% 1) create the bathymetry file
-% -----------------------------
+% 1) try to obtain the elevation in different ways
+% ------------------------------------------------
 
-bb=1; % 1 degree of bathy outside centroids (to allow for smooth interp)
-bathy_coords=[centroids_rect(1)-bb centroids_rect(2)+bb centroids_rect(3)-bb centroids_rect(4)+bb];
-
-% cut the bathymtery data out of the global topography dataset
-
-% file the bathymetry gets stored in ( might not be used later on, but
-% saved to speed up re-creation of the TS hazard event set
-[~,fN]=fileparts(hazard_set_file);
-Bathymetry_file=[module_data_dir filesep fN '_bathy.mat'];
-
-if ~exist(Bathymetry_file,'file')
-    
-    if ~exist('etopo_get','file')
-        % safety to inform the user in case he misses the ETOPO module
-        cprintf([1,0.5,0],'ERROR: no etopo_get function found. Please download from github and install the climada etopo module\nhttps://github.com/davidnbresch/climada_module_etopo\n');
-        hazard=[];
-        return
+if ~isfield(hazard,'elevation_m') && ~isempty(elevation_data)
+    % first, use data as provided in elevation_data
+    if isfield(elevation_data,'centroid_ID')
+        if length(elevation_data.centroid_ID)==length(hazard.centroid_ID)
+            if sum(elevation_data.centroid_ID-hazard.centroid_ID)==0
+                % elevation provided at hazard centroids
+                hazard.elevation_m=elevation_data.elevation_m;
+            end
+        end
     end
-    BATI=etopo_get(bathy_coords);
-    if isempty(BATI),hazard=[];return;end % error messages from etopo_get already
+end
+
+if ~isfield(hazard,'elevation_m')
     
-    if save_bathymetry_flag
-        fprintf('saving bathymetry as %s (you might later delete this file)\n',Bathymetry_file);
-        save(Bathymetry_file,'BATI');
+    if ~isempty(elevation_data)
+        % if elevation_data provided, we should not get here
+        fprintf('Warning: elevation_data failed, using etopo\n');
     end
     
-    if check_plots
-        figure('Name','Bathymetry','Color',[1 1 1]);
-        pcolor(BATI.x,BATI.y,BATI.h)
-        hold on
-        shading interp
-        colorbar
-        caxis([-200 20]) % a reasonable colorbar for elevation
-        axis equal
-        climada_plot_world_borders
-        axis(bathy_coords);
+    % second, if elevation_data is empty, use the etopo module
+    
+    % prep the region we need (rectangular region encompassing the hazard centroids)
+    centroids_rect=[min(hazard.lon) max(hazard.lon) min(hazard.lat) max(hazard.lat)];
+    
+    % 1) create the bathymetry file
+    % -----------------------------
+    
+    bb=1; % 1 degree of bathy outside centroids (to allow for smooth interp)
+    bathy_coords=[centroids_rect(1)-bb centroids_rect(2)+bb centroids_rect(3)-bb centroids_rect(4)+bb];
+    
+    % cut the bathymtery data out of the global topography dataset
+    
+    % file the bathymetry gets stored in ( might not be used later on, but
+    % saved to speed up re-creation of the TS hazard event set
+    [~,fN]=fileparts(hazard_set_file);
+    Bathymetry_file=[module_data_dir filesep fN '_bathy.mat'];
+    
+    if ~exist(Bathymetry_file,'file')
+        
+        if ~exist('etopo_get','file')
+            % safety to inform the user in case he misses the ETOPO module
+            cprintf([1,0.5,0],'ERROR: no etopo_get function found. Please download from github and install the climada etopo module\nhttps://github.com/davidnbresch/climada_module_etopo\n');
+            hazard=[];
+            return
+        end
+        BATI=etopo_get(bathy_coords);
+        if isempty(BATI),hazard=[];return;end % error messages from etopo_get already
+        
+        if save_bathymetry_flag
+            fprintf('saving bathymetry as %s (you might later delete this file)\n',Bathymetry_file);
+            save(Bathymetry_file,'BATI');
+        end
+        
+        if check_plots
+            figure('Name','Bathymetry','Color',[1 1 1]);
+            pcolor(BATI.x,BATI.y,BATI.h)
+            hold on
+            shading interp
+            colorbar
+            caxis([-200 20]) % a reasonable colorbar for elevation
+            axis equal
+            climada_plot_world_borders
+            axis(bathy_coords);
+        end
+        
+    else
+        fprintf('reading bathymetry from %s\n',Bathymetry_file);
+        load(Bathymetry_file); % contains BATI
     end
     
-    % netcdf.close(bathy_data_file) % seems not be required
-else
-    fprintf('reading bathymetry from %s\n',Bathymetry_file);
-    load(Bathymetry_file);
+    hazard.elevation_m=interp2(BATI.x,BATI.y,BATI.h,hazard.lon,hazard.lat);
+    
 end
 
 % 2) create the storm surge (TS) hazard event set
@@ -142,13 +188,16 @@ end
 % start from the 'mother' TC hazard event set
 
 if isfield(hazard,'onLand'),hazard=rmfield(hazard,'onLand');end % re-create to be on the safe side
-hinterp=interp2(BATI.x,BATI.y,BATI.h,hazard.lon,hazard.lat);
 hazard.onLand=hazard.lon.*0+1; % allocate
-hazard.onLand(hinterp<0)=0; % water points
+hazard.onLand(hazard.elevation_m<0)=0; % water points
 
 hazard.peril_ID='TS'; % replace TC with TS
 if isfield(hazard,'windfield_comment'),hazard=rmfield(hazard,'windfield_comment');end % remove
-hazard.surgefield_comment=sprintf('created based on TC using proxy surge height and bathymetry %s',BATI.sourcefile);
+if exist('BATI','var')
+    hazard.surgefield_comment=sprintf('created based on TC using proxy surge height and bathymetry %s',BATI.sourcefile);
+else
+    hazard.surgefield_comment=sprintf('created based on TC using proxy surge height and elevation_m');
+end
 hazard.comment=sprintf('TS hazard event set, generated %s',datestr(now));
 
 % map windspeed onto surge height (the CORE_CONVERSION, see at botton of file, too)
@@ -156,13 +205,7 @@ hazard.comment=sprintf('TS hazard event set, generated %s',datestr(now));
 arr_nonzero=find(hazard.intensity); % to avoid de-sparsify all elements
 hazard.intensity(arr_nonzero)=0.1023*(max(hazard.intensity(arr_nonzero)-26.8224,0))+1.8288; % m/s converted to m surge height
 
-% grid bathymetry to centroids (convert n x m BATI to vectors in call)
-xx=double(reshape(BATI.x,numel(BATI.x),1));
-yy=double(reshape(BATI.y,numel(BATI.y),1));
-zz=double(reshape(BATI.h,numel(BATI.h),1));
-hazard.bathy=griddata(xx,yy,zz,hazard.lon,hazard.lat);
-
-hazard.elev=max(hazard.bathy,0); % only points above sea level
+hazard.elevation_m=max(hazard.elevation_m,0); % only points above sea level
 
 % subtract elevation above sea level from surge height
 t0       = clock;
@@ -186,7 +229,7 @@ if n_events<n_centroids % loop over events, since less events than centroids
     
     for event_i=1:n_events
         arr_i=find(hazard.intensity(event_i,:)); % to avoid de-sparsify all elements
-        hazard.intensity(event_i,arr_i)=max(hazard.intensity(event_i,arr_i)-hazard.elev(arr_i),0);
+        hazard.intensity(event_i,arr_i)=max(hazard.intensity(event_i,arr_i)-hazard.elevation_m(arr_i),0);
         
         if mod(event_i,mod_step)==0
             mod_step = 100;
@@ -225,7 +268,7 @@ else % loop over centroids, since less centroids than events
     
     for centroid_i=1:n_centroids
         arr_i=find(hazard.intensity(:,centroid_i)); % to avoid de-sparsify all elements
-        hazard.intensity(arr_i,centroid_i)=max(hazard.intensity(arr_i,centroid_i)-hazard.elev(centroid_i),0);
+        hazard.intensity(arr_i,centroid_i)=max(hazard.intensity(arr_i,centroid_i)-hazard.elevation_m(centroid_i),0);
         
         if mod(centroid_i,mod_step)==0
             mod_step = 100;
